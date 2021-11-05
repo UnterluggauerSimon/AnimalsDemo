4262c6771b46b654edf632b4291d566d069c618b
Simon Unterluggauer <s.unterluggauer@navax.com>
2021-11-05 20:53:11 +0100

    Initial commit

---
+++ src/main/java/at/unterluggauer/Animal.java
@@ -0,0 +1,5 @@
+package at.unterluggauer;
+
+public class Animal
+{
+}
---
+++ src/test/java/at/unterluggauer/AnimalTest.java
@@ -0,0 +1,24 @@
+package at.unterluggauer;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AnimalTest
+{
+    int a;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp()
+    {
+        a = 1;
+    }
+
+    @Test
+    void myFirstTest()
+    {
+        assertTrue(a > 0);
+    }
+}
